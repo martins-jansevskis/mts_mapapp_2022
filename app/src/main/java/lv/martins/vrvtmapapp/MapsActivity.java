4a3ccb9d531016b5ffd3eb2e1f6254f4d854c00b
Martins <martins.jansevskis@gmail.com>
2020-10-31 11:52:40 +0200

    Added functionality

--- app/src/main/java/lv/martins/vrvtmapapp/MapsActivity.java
+++ app/src/main/java/lv/martins/vrvtmapapp/MapsActivity.java
@@ -3,7 +3,11 @@ package lv.martins.vrvtmapapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -42,8 +46,29 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(57.536393, 25.424059);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Valmiera"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.getplaces:
+                startActivity(new Intent(MapsActivity.this, ReadNewsActivity.class));
+                return true;
+            case R.id.readnews:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
